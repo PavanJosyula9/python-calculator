6d7026b67aa17f64fa17c61c31ce540db9d7242b
Pavan Josyula <josyulapavan99@gmail.com>
2026-04-29 00:49:55 +0530

    feat: add history, stateful calculations, and improved CLI flow

--- main.py
+++ main.py
@@ -1,43 +1,82 @@
 from operations import add, subtract, multiply, divide
+from utils.input_handler import get_choice, get_number
+from history.manager import add_entry, show_history
+
+def handle_result(num1: float, num2: float, symbol: str, result: float):
+    entry: str = f"{num1} {symbol} {num2} = {result}"
+    add_entry(entry)
+    print('Result:', result)
+    print()
 
 def main():
+    current_result: float | None = None
+
     while True:
-        try:
-            print('Select operation:')
-            print('1. Add')
-            print('2. Subtract')
-            print('3. Multiply')
-            print('4. Divide')
-            print('5. Exit')
+        print('Select operation:')
+        print('1. Add')
+        print('2. Subtract')
+        print('3. Multiply')
+        print('4. Divide')
+        print('5. Exit')
+        print('6. Show History')
 
-            choice: int = int(input("Enter your choice: "))
+        choice: int = get_choice('Enter your choice: ')
 
-            if choice not in range(1,6):
-                print('Please choose valid options!')
-                continue
+        if choice not in range(1, 7):
+            print('Please choose valid options!')
+            print()
+            continue
 
-            if choice == 5:
-                print('Goodbye!')
-                break
+        if choice == 5:
+            print('Goodbye!')
+            break
 
-            
-            num1: float = float(input('Enter your first number: '))
-            num2: float = float(input('Enter your second number: '))
+        if choice == 6:
+            show_history()
+            print()
+            continue
 
-            if choice == 1:
-                print('Result:', add(num1, num2))
-            elif choice == 2:
-                print('Result:', subtract(num1, num2))
-            elif choice == 3:
-                print('Result:', multiply(num1, num2))
-            elif choice == 4:
-                try:
-                    result = divide(num1, num2)
-                    print('Result:', result)
-                except ValueError as e:
-                    print(e)
-        except ValueError:
-            print('Invalid input! Please enter numbers only.')
+        if current_result is None:
+            num1: float = get_number('Enter your first number: ')
+        else:
+            print(f"Current result: {current_result}")
+            while True:
+                use_prev: str = input('Continue with this result? (y/n): ').lower()
+
+                if use_prev == 'y':
+                    num1 = current_result
+                    break
+                elif use_prev == 'n':
+                    current_result = None
+                    num1 = get_number('Enter your first number: ')
+                    break
+                else:
+                    print("Please enter either 'y' or 'n'...")                
+
+        num2: float = get_number('Enter your second number: ')
+
+        
+
+        if choice == 1:
+            result: float = add(num1, num2)
+            current_result = result
+            handle_result(num1, num2, '+', result)
+        elif choice == 2:
+            result: float = subtract(num1, num2)
+            current_result = result
+            handle_result(num1, num2, '-', result)
+        elif choice == 3:
+            result: float = multiply(num1, num2)
+            current_result = result
+            handle_result(num1, num2, '*', result)
+        elif choice == 4:
+            try:
+                result = divide(num1, num2)
+                current_result = result
+                handle_result(num1, num2, '/', result)
+            except ValueError as e:
+                print(e)
+                print()
 
 
 if __name__ == '__main__':
